98c849ce83d8f096698a382d6d40ff43ea56969d
codewithgaji <gajiyakub6@gmail.com>
2025-02-22 14:03:45 +0100

    Added SensAi files

--- Desktop/Mental Health Care Project/vibecheck/journal/views.py
+++ Desktop/Mental Health Care Project/vibecheck/journal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .models import JournalEntry
+from .serializers import JournalENtrySerializer
+from .utils import analyze_sentiment
 
 # Create your views here.
+class JournalEntryView(APIViews):
+    journals = JournalEntry.objects.filter(user=request.user).order_by()
+
